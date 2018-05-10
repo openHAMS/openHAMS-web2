573dbdbc82b40fb7de14dd3db837ac9d6acdc464
Laszlo Szekely-Toth <laszlo_szekely-toth@epam.com>
2018-05-10 17:22:13 +0200

    :construction: Added basic header branding

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,11 @@
 <template lang="pug">
     v-app
-        v-navigation-drawer(app)
-        v-toolbar(app)
         v-content
-            v-container(fluid)
-                router-view
+            v-container
+                v-layout
+                    v-flex
+                        h1.branding.accent--text openHAMS
+            router-view
         v-footer(app)
 </template>
 
