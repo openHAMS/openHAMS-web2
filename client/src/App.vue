dfc65b2bfb133aeaa6632d15e1eff94466bb6f36
Laszlo Szekely-Toth <laszlo_szekely-toth@epam.com>
2018-05-11 17:39:36 +0200

    :art: Extracted brand header into separate .vue component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,41 +1,21 @@
 <template lang="pug">
     v-app
         v-content
-            v-container
-                v-layout
-                    v-flex
-                        h1.branding.accent--text openHAMS
+            brand-header
             router-view
-        v-footer(app)
 </template>
 
 <script>
+import BrandHeader from './components/BrandHeader.vue';
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        BrandHeader
+    }
 };
 </script>
 
 <style lang="scss">
 @import "../node_modules/vuetify/dist/vuetify.css";
-@import "./assets/breakpoints.scss";
-
-.branding {
-    @include lg-and-up {
-        font-size: 6rem;
-    }
-    @include md-and-down {
-        font-size: 5rem;
-        line-height: 1;
-    }
-    @include sm-and-down {
-        font-size: 4rem;
-        //margin: 0.3em;
-    }
-    @include xs-and-down {
-        //line-height: 0.8;
-    }
-    font-weight: 300;
-    //line-height: 1 !important;
-    letter-spacing: -0.04em !important;
-}
 </style>
---
+++ client/src/components/BrandHeader.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+    div.brand-container
+        h1.branding.accent--text openHAMS
+</template>
+
+<script>
+export default {
+    name: 'Header'
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/breakpoints.scss";
+
+.brand-container {
+    margin: auto;
+    padding: 16px;
+    width: 100%;
+    @include sm-only {
+        padding-left: 24px;
+    }
+    @include md-only {
+        max-width: 900px;
+    }
+    @include lg-only {
+        max-width: 1185px;
+    }
+    @include xl-and-up {
+        max-width: 1785px;
+    }
+}
+
+.branding {
+    font-weight: 300;
+    letter-spacing: -0.04em !important;
+    user-select: none; 
+    @include lg-and-up {
+        font-size: 6rem;
+    }
+    @include lg-only {
+        line-height: 1.2;
+    }
+    @include md-and-down {
+        font-size: 5.5rem;
+        line-height: 1;
+    }
+    @include sm-only {
+        line-height: 1.2;
+    }
+    @include xs-and-down {
+        font-size: 4rem;
+    }
+}
+</style>
