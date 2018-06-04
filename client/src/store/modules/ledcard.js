3d56934846580b8a84fd8e3649c6270717f3a2ac
Laszlo Szekely-Toth <laszlo_szekely-toth@epam.com>
2018-06-04 16:33:24 +0200

    :heavy_plus_sign: Added vuex

---
+++ client/src/store/modules/ledcard.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const state = {
+    
+};
+
+const getters = {
+    
+};
+
+const actions = {
+    
+};
+
+const mutations = {
+    
+};
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+};
