e89a6db28f2a2bc02fe1173d2a6b8858c825d0a4
Laszlo Szekely-Toth <notisracer@gmail.com>
2018-06-28 17:30:24 +0200

    :bug: Forgot to commit settings vuex change

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import dashboard from './modules/dashboard';
-import theme from './modules/theme';
+import settings from './modules/settings';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         dashboard,
-        theme,
+        settings,
     },
     strict: true,
     plugins: [
