df0c6cd4c9753e2808bf9106338e0815230f9342
Székely-Tóth László <notisracer@gmail.com>
2018-11-19 15:52:25 +0100

    :sparkles: Added persisting vuex

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import omit from 'lodash-es/omit';
 import { storeApiPlugin } from './api';
+import createPersistedState from 'vuex-persistedstate';
 import dashboard from './components/dashboard/DashboardStoreModule';
 import settings, { CHECK_AUTH } from './components/settings';
 
@@ -15,5 +17,18 @@ export default new Vuex.Store({
     plugins: [
         storeApiPlugin,
         store => store.dispatch(CHECK_AUTH),
+        createPersistedState({
+            reducer: (state) => {
+                // persist all except internal states and JWT
+                const internal = [
+                    'error',
+                    'isLoading',
+                ];
+                const excluded = [
+                    'jwt',
+                ];
+                return omit(state.settings, [...internal, ...excluded]);
+            },
+        }),
     ],
 });
