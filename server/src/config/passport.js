6c5731ff6a7b6e88228caf03f0186a64f7bef40d
Székely-Tóth László <notisracer@gmail.com>
2018-11-19 14:44:05 +0100

    :sparkles: Implemented JWT strategy

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -12,6 +12,7 @@ function setupUserSerialization() {
         done(null, user);
     });
 }
+import { JwtStrategy } from './passport/strategies/jwt';
 
 export default ({ app }) => {
     setupUserSerialization();
@@ -20,5 +21,7 @@ export default ({ app }) => {
     // google auth
     passport.use(GoogleStrategy);
     app.use('/auth/google', GoogleRouter);
+    // jwt auth
+    passport.use(JwtStrategy);
     // NOTE: passport is singleton, so every require/import returns the _same_ passport instance
 };
