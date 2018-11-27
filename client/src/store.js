2091ca4f08430e7ff7f5cc0943d44f72167b393d
Székely-Tóth László <notisracer@gmail.com>
2018-11-27 12:41:31 +0100

    :sparkles: Modified vuex-persistedstate's getState to load cached state only if token present; else delete cache

--- client/src/store.js
+++ client/src/store.js
@@ -18,6 +18,23 @@ export default new Vuex.Store({
         storeApiPlugin,
         store => store.dispatch(CHECK_AUTH),
         createPersistedState({
+            getState: (key, storage) => {
+                // default getState fn from https://github.com/robinvdvleuten/vuex-persistedstate/blob/master/index.js
+                // refactored & modified to check JWT token
+                const value = storage.getItem(key);
+                if (!value) {
+                    return undefined;
+                }
+                try {
+                    if (!localStorage.getItem('token')) {
+                        throw new Error('Store inconsistency error. Persisted vuex state present without token.');
+                    }
+                    return JSON.parse(value);
+                } catch (err) {
+                    storage.removeItem(key);
+                    return undefined;
+                }
+            },
             reducer: (state) => {
                 // persist all except internal states and JWT
                 const internal = [
