016ebfe677ea4da62f9ef344729331e0b3503c62
Székely-Tóth László <notisracer@gmail.com>
2018-11-29 18:11:49 +0100

    :art: :white_check_mark: Removed unnecessary jest-localstorage-mock setup file (jest calls module directly in setupfiles)

--- client/jest.config.js
+++ client/jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'json',
     ],
     setupFiles: [
+        'jest-localstorage-mock',
         '<rootDir>/test/jest-fetch-mock.js',
-        '<rootDir>/test/jest-localstorage-mock.js',
     ],
 };
