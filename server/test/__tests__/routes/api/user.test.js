a8881dbf341930e99f5a1c2429a85c34484a8be6
Székely-Tóth László <notisracer@gmail.com>
2018-12-14 13:16:49 +0100

    :art: Updated jest-express and rewrote server userApi test to use jest-express router

--- server/test/__tests__/routes/api/user.test.js
+++ server/test/__tests__/routes/api/user.test.js
@@ -1,15 +1,17 @@
-import { flatten } from 'lodash';
-
 // setup express & express.Router mock
 jest.mock('express', () => ({
+    ...require('jest-express'),
     Router: jest.fn(),
 }));
 import express from 'express';
 import { Request, Response } from 'jest-express';
-const router = {
-    get: jest.fn(),
-    use: jest.fn(),
+// create single Router mock instance
+const router = require('jest-express').Router();
+// setup user methods
+router.request.user = {
+    toApiObject: jest.fn(),
 };
+// set mock to return always the same router instance
 express.Router.mockImplementation(() => router);
 
 // setup passport mock
@@ -20,41 +22,34 @@ import passport from 'passport';
 const authenticateObject = new Object();
 passport.authenticate.mockImplementation(() => authenticateObject);
 
-// use require to ensure synchronity
-const userRouter = require('@/routes/api/user').default;
+// setup settings router mock
+jest.mock('@/routes/api/settings', () => jest.fn());
 
-function getRouterRoutes(httpMethod) {
-    const httpMethodCalls = router[httpMethod].mock.calls;
-    return httpMethodCalls.reduce((acc, call) => {
-        const [route, handler] = call;
-        acc[route] = handler;
-        return acc;
-    }, {});
-}
+// use require to ensure synchronous (immediate) load
+const {
+    default: userRouter,
+    getUser,
+} = require('@/routes/api/user');
+
 
 describe('User api route', () => {
-    const routeMap = {
-        get: getRouterRoutes('get'),
-    };
-
     it('returns Router', () => {
         expect(userRouter).toBe(express.Router());
     });
 
     describe('GET /', () => {
         it('exists', () => {
-            expect(routeMap.get).toHaveProperty('/', expect.any(Function));
+            expect(router.get).toHaveBeenCalledWith('/', getUser);
         });
 
         it('calls res.status(200).json() with the value of req.user.toApiObject()', () => {
-            const rootHandler = routeMap.get['/'];
             const apiObject = new Object();
             const req = new Request('/');
             req.user = {
                 toApiObject: jest.fn(() => apiObject),
             };
             const res = new Response();
-            rootHandler(req, res);
+            getUser(req, res);
             expect(res.status).toHaveBeenCalledTimes(1);
             expect(res.status).toHaveBeenCalledWith(200);
             expect(res.json).toHaveBeenCalledTimes(1);
