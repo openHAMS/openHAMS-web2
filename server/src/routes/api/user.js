db16e8cebff5da4bbcccf2bdf84383c5a569e9e9
Székely-Tóth László <notisracer@gmail.com>
2018-12-14 13:10:32 +0100

    :art: Separated server api routes and implementations

--- server/src/routes/api/user.js
+++ server/src/routes/api/user.js
@@ -3,9 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
+export const getUser = (req, res) => {
     const profile = req.user.toApiObject();
     res.status(200).json(profile);
-});
+};
+router.get('/', getUser);
 
 export default router;
